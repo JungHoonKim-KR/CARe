1bdd009233d349a42748bd84f5de976cfcc44536
손효지 <hyoji0284@naver.com>
2026-03-17 15:52:58 +0900

    fe | feat : 임대인 차량 예약 내역 페이지 구현

--- company/src/App.jsx
+++ company/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ReservationPage from './pages/reservation/ReservationPage'
+import './App.css'
 
 export default function App() {
   return (
-    <div>
-      <h1>Company App</h1>
+    <div className="app-container">
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Navigate to="/reservations" replace />} />
+          <Route path="/reservations" element={<ReservationPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }

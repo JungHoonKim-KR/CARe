9ad2d2783bb2e2a8b4eec9b2e7b4d37351ab5c57
김정훈 <kik1232198@gmail.com>
2026-03-18 22:56:41 +0900

    bc | feat: hardhat 설정 변경

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x" + "a".repeat(64);
+const SSAFY_RPC_URL = process.env.SSAFY_RPC_URL || "https://rpc.ssafy-blockchain.com";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  networks: {
+    localhost: { url: "http://127.0.0.1:8545" },
+    ssafy: {
+      url: SSAFY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 31221,
+    },
+  },
 };

1c323d4911913574465994bd2228e82f14652c4a
김정훈 <kik1232198@gmail.com>
2026-03-19 00:28:29 +0900

    be | feat: Company, Renter 에러 코드 정의

---
+++ backend/src/main/java/com/care/domain/company/exception/CompanyErrorCode.java
@@ -0,0 +1,16 @@
+package com.care.domain.company.exception;
+
+import com.care.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CompanyErrorCode implements ErrorCode {
+
+    COMPANY_NOT_FOUND("존재하지 않는 회사입니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final HttpStatus httpStatus;
+}

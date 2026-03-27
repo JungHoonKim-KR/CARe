158dfe5ae90a654ddb61244ba5daa1c1c0d4ce86
김정훈 <kik1232198@gmail.com>
2026-03-28 01:42:28 +0900

    배포

--- backend/src/main/java/com/care/CareApplication.java
+++ backend/src/main/java/com/care/CareApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 
-
 public class CareApplication {
     public static void main(String[] args) {
         SpringApplication.run(CareApplication.class, args);

dc0ecc71455563237de630d70abff4b69560a221
김정훈 <kik1232198@gmail.com>
2026-03-27 19:12:09 +0900

    be | fix : 배포

--- backend/src/main/java/com/care/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/care/domain/auth/controller/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
